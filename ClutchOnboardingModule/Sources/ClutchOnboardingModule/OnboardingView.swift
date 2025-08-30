b8fa7dfa45d36ca7b23362c37eb2c150f3d280c9
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-08-30 16:18:55 +0300

    second base design finished
    added image and changed design. Added blur for images

--- ClutchOnboardingModule/Sources/ClutchOnboardingModule/OnboardingView.swift
+++ ClutchOnboardingModule/Sources/ClutchOnboardingModule/OnboardingView.swift
@@ -18,77 +18,91 @@ struct OnboardingView<VM:OnboardingViewModelProtocol>: View {
     @State private var currentPage = 0
     
     var body: some View {
-        VStack {
-            Spacer()
+        
+        ZStack {
             
-            
-            Image(systemName: viewModel.getImageName())
+            Image(.onboardingOne)
                 .resizable()
-                .scaledToFit()
-                .frame(width: 150, height: 150)
-                .padding()
+                .scaledToFill()
+                .blur(radius: 6)
+                .ignoresSafeArea()
             
-            
-            Text(viewModel.pages[currentPage].title)
-                .font(.title2)
-                .multilineTextAlignment(.center)
-                .padding()
-            
-            Spacer()
-            
-            HStack {
+           
+            VStack(alignment:.center,spacing: 20) {
+                Image(.onboardingOne)
+                           .resizable()
+                           .scaledToFill()
+                           .frame(width: 250, height: 350)
+                           .clipShape(RoundedRectangle(cornerRadius: 60, style: .continuous))
+                           .overlay(
+                               RoundedRectangle(cornerRadius: 60, style: .continuous)
+                                   .stroke(Color.white, lineWidth: 5)
+                                   .shadow(radius: 5)
+                           ).padding(.top,10)
                 
-                viewModel.leftArrowButtonHidden ?
-                Button(action: {
-                    viewModel.currentPageDecrement()
-                    
-                }) {
-                    Image(systemName: "arrow.left.circle.fill")
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                        .foregroundColor(.blue)
-                        .padding(.trailing, 20)
-                } : nil
+                Text("Welcome to Clutch")
+                    .font(.title2)
+                    .foregroundStyle(.white)
+                    .multilineTextAlignment(.center)
+                    .fontWeight(.semibold)
+                  
                 
                 
-                
-                HStack(spacing: 8) {
-                    ForEach(0..<viewModel.pages.count, id: \.self) { index in
-                        Circle()
-                            .fill(index == viewModel.currentPage ? Color.blue : Color.gray.opacity(0.4))
-                            .frame(width: 10, height: 10)
-                    }
-                }
-                .padding(.leading, 20)
+                Text("It is a long established fact that a reader will be  ")
+                    .multilineTextAlignment(.center)
+                    .font(.headline)
+                    .foregroundStyle(.white)
+                  
+                    .lineLimit(.max)
+                    .padding(.horizontal,60)
+                  
+                  
                 
                 Spacer()
                 
-                
-                
-                viewModel.goToSingPage ? AnyView(
+                HStack {
+                    
                     
                     Button(action: {
-                       
+                         viewModel.onTappedSkip()
+                     }) {
+                         
+                         Text("Skip")
+                            
+                             .font(.title2)
+                             .foregroundStyle(.white)
+                             .multilineTextAlignment(.center)
+                             .fontWeight(.light)
+                            
+                     }
+                    
+                  
+                    HStack(spacing: 8) {
+                        ForEach(0..<viewModel.pages.count, id: \.self) { index in
+                            Circle()
+                                .fill(index == viewModel.currentPage ? Color.white : Color.gray.opacity(0.4))
+                                .frame(width: 10, height: 10)
+                        }
+                    }.padding(.horizontal,90)
+                    
+                   Button(action: {
+                        viewModel.currentPageIncrement()
                     }) {
-                        Text("Go To Sing PAge")
+                        
+                        Text(viewModel.rightText)
+                               .font(.title2)
+                               .foregroundStyle(.white)
+                               .multilineTextAlignment(.center)
+                               .fontWeight(.bold)
+                           
                     }
-                ) : AnyView(Button(action: {
-                    viewModel.currentPageIncrement()
-                }) {
-                    Image(systemName: "arrow.right.circle.fill")
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                        .foregroundColor(.blue)
-                        .padding(.trailing, 20)
-                })
-                
-                
-                
-                
+                 
+                }
+               
             }
-            .padding(.bottom, 30)
         }
-        .animation(.easeInOut, value: viewModel.currentPage)
+        
+   
     }
 }
 
--- ClutchOnboardingModule/Sources/ClutchOnboardingModule/OnboardingViewModel.swift
+++ ClutchOnboardingModule/Sources/ClutchOnboardingModule/OnboardingViewModel.swift
@@ -10,48 +10,44 @@ import Foundation
 protocol OnboardingViewModelProtocol: ObservableObject {
     var currentPage: Int { get }
     var pages: [OnboardingPage] { get }
-    var goToSingPage: Bool { get }
-    var leftArrowButtonHidden: Bool { get }
+    var rightText: String { get }
+    
     
     func currentPageIncrement()
-    func currentPageDecrement()
-    func getImageName() -> String
+    
+    func getOnboardingPage() -> OnboardingPage
+    func onTappedSkip()
 }
 
 class OnboardingViewModel: OnboardingViewModelProtocol {
     
     @Published private(set) var currentPage: Int = 0
-    @Published private(set) var goToSingPage: Bool = false
-    @Published private(set) var leftArrowButtonHidden: Bool = false
+    @Published private(set) var rightText: String = "Next"
+    
     
     let pages: [OnboardingPage] = [
-        OnboardingPage(image: "star", title: "Uygulamamıza Hoşgeldin!"),
-        OnboardingPage(image: "heart", title: "Favori içeriklerini kaydet."),
-        OnboardingPage(image: "bolt", title: "Hızlı ve kolay kullanım.")
+        OnboardingPage(image: "onboardingOne", title: "Uygulamamıza Hoşgeldin!"),
+        OnboardingPage(image: "onboardingOne", title: "Favori içeriklerini kaydet."),
+        OnboardingPage(image: "onboardingOne", title: "Hızlı ve kolay kullanım.")
     ]
     
     func currentPageIncrement() {
        
         if currentPage < pages.count - 1 {
             currentPage += 1
-            updateState()
+            rightText = (currentPage == pages.count - 1) ? "Login" : "Next"
         }
     }
     
-    func currentPageDecrement() {
-        if currentPage > 0 {
-            currentPage -= 1
-            updateState()
-        }
+   
+    
+    func getOnboardingPage() -> OnboardingPage {
+        return pages[currentPage]
     }
     
-    func getImageName() -> String {
-        return pages[currentPage].image
+    func onTappedSkip() {
+        print("On Tappped Skip")
     }
-    
-     func updateState() {
-        leftArrowButtonHidden = (currentPage > 0)
-        goToSingPage = (currentPage == pages.count - 1)
-    }
+   
 }
 
